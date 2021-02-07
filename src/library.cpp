8a93c94bf70b210c73481e82316a1020a1f2435d
xqq <xqq@xqq.im>
2021-02-07 18:27:21 +0900

    library.cpp: Add DllMain & yaml loading logic

---
+++ src/library.cpp
@@ -0,0 +1,71 @@
+#ifdef _WIN32
+    #include <Windows.h>
+#endif
+
+#include <fstream>
+#include <yaml-cpp/yaml.h>
+#include "IBonDriver.h"
+#include "bon_driver.hpp"
+#include "config.hpp"
+#include "log.hpp"
+#include "library.hpp"
+
+static HMODULE hmodule = nullptr;
+static Config config;
+
+static bool LoadConfigYamlFile() {
+    Log::InfoF(LOG_FILE_FUNCTION);
+    // Get dll file path
+    char path_buffer[_MAX_PATH] = {0};
+    if (GetModuleFileNameA(hmodule, path_buffer, _MAX_PATH) == 0) {
+        DWORD ret = GetLastError();
+        Log::ErrorF("GetModuleFileName failed, error = %#010x", ret);
+        return false;
+    }
+
+    char drive[_MAX_DRIVE] = {0};
+    char dir[_MAX_DIR] = {0};
+    char fname[_MAX_FNAME] = {0};
+    char ext[_MAX_EXT] = {0};
+
+    _splitpath_s(path_buffer, drive, dir, fname, ext);
+
+    // Generate yaml file path from dll path
+    std::string yaml_path(_MAX_PATH, '\0');
+    sprintf(&yaml_path[0], "%s%s%s.yml", drive, dir, fname);
+    Log::InfoF("Yaml FilePath: %s", yaml_path.c_str());
+
+    return config.LoadYamlFile(yaml_path);
+}
+
+extern "C" EXPORT_API IBonDriver* CreateBonDriver() {
+    Log::InfoF(LOG_FILE_FUNCTION);
+
+    if (!config.IsLoaded()) {
+        if (!LoadConfigYamlFile()) {
+            return nullptr;
+        }
+    }
+
+    return new BonDriver(config);
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) {
+    switch(fdwReason) {
+        case DLL_PROCESS_ATTACH:
+            Log::Info("DLL_PROCESS_ATTACH");
+            hmodule = hinstDLL;
+            LoadConfigYamlFile();
+            break;
+        case DLL_THREAD_ATTACH:
+            Log::Info("DLL_THREAD_ATTACH");
+            break;
+        case DLL_THREAD_DETACH:
+            Log::Info("DLL_THREAD_DETACH");
+            break;
+        case DLL_PROCESS_DETACH:
+            Log::Info("DLL_PROCESS_DETACH");
+            break;
+    }
+    return TRUE;
+}
