d5096b4f9b1b3137816fcc09c487efaa69d79550
xqq <xqq@xqq.im>
2021-02-16 01:17:04 +0900

    library.cpp: Remove thread attach/detach logcat

--- src/library.cpp
+++ src/library.cpp
@@ -61,12 +61,6 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved) {
             hmodule = hinstDLL;
             LoadConfigYamlFile();
             break;
-        case DLL_THREAD_ATTACH:
-            Log::Info("DLL_THREAD_ATTACH");
-            break;
-        case DLL_THREAD_DETACH:
-            Log::Info("DLL_THREAD_DETACH");
-            break;
         case DLL_PROCESS_DETACH:
             Log::Info("DLL_PROCESS_DETACH");
             break;
