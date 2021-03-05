f5dc0871a5dcd544098da4275846a826de3ec34e
xqq <xqq@xqq.im>
2021-03-05 22:00:28 +0900

    EPGStationAPI: Migrate http library to cpr

--- src/epgstation_api.cpp
+++ src/epgstation_api.cpp
@@ -2,15 +2,13 @@
 // @author magicxqq <xqq@xqq.im>
 //
 
-#include <cpprest/http_client.h>
+#include <cpr/cpr.h>
 #include <nlohmann/json.hpp>
 #include "epgstation_models_deserialize.hpp"
 #include "log.hpp"
 #include "utils.hpp"
 #include "epgstation_api.hpp"
 
-using namespace web::http;
-using namespace web::http::client;
 using json = nlohmann::json;
 
 static const char* kEPGStationAPIConfig = "/api/config";
@@ -27,86 +25,53 @@ bool EPGStationAPI::SetBasicAuth(const std::string& user, const std::string& pas
 }
 
 std::optional<EPGStation::Config> EPGStationAPI::GetConfig() {
-    http_client_config client_config;
+    cpr::Url url{this->base_url_ + kEPGStationAPIConfig};
+    cpr::Response response;
 
-    if (has_basic_auth_) {
-        web::credentials credentials(UTF8ToPlatformString(basicauth_user_), UTF8ToPlatformString(basicauth_password_));
-        client_config.set_credentials(credentials);
+    if (!has_basic_auth_) {
+        response = cpr::Get(url);
+    } else {
+        cpr::Authentication auth{ basicauth_user_, basicauth_password_ };
+        response = cpr::Get(url, auth);
     }
 
-    http_client client(UTF8ToPlatformString(base_url_), client_config);
-    bool succeed = false;
-    bool error = false;
-    EPGStation::Config config;
-
-    pplx::task<void> request_task = pplx::create_task([&]() {
-        // Request
-        return client.request(methods::GET, UTF8ToPlatformString(kEPGStationAPIConfig));
-    }).then([&](const http_response& response) {
-        // Handle response
-        if (response.status_code() != status_codes::OK) {
-            error = true;
-        }
-        return response.extract_utf8string();
-    }).then([&](const std::string& response_string) {
-        // Handle JSON
-        if (error) {
-            Log::ErrorF("%s failed: %s", kEPGStationAPIConfig, response_string.c_str());
-            return;
-        }
-
-        json j = json::parse(response_string);
-        config = j.get<EPGStation::Config>();
-        succeed = true;
-    });
-
-    try {
-        request_task.wait();
-    } catch (const std::exception& ex) {
-        Log::ErrorF(LOG_FILE_FUNCTION_MESSAGE(ex.what()));
+    if (response.error) {
+        Log::ErrorF("curl failed for %s: error_code = %d, msg = %s", kEPGStationAPIConfig, response.error.code, response.error.message.c_str());
+        return std::nullopt;
+    } else if (response.status_code >= 400) {
+        Log::ErrorF("%s error: status_code = %d, body = %s", kEPGStationAPIConfig, response.status_code, response.text.c_str());
+        return std::nullopt;
     }
 
-    return succeed ? std::make_optional(config) : std::nullopt;
+    json j = json::parse(response.text);
+    EPGStation::Config config = j.get<EPGStation::Config>();
+
+    return config;
 }
 
-
 std::optional<EPGStation::Channels> EPGStationAPI::GetChannels() {
-    http_client_config client_config;
+    cpr::Url url{this->base_url_ + kEPGStationAPIChannels};
+    cpr::Response response;
 
-    if (has_basic_auth_) {
-        web::credentials credentials(UTF8ToPlatformString(basicauth_user_), UTF8ToPlatformString(basicauth_password_));
-        client_config.set_credentials(credentials);
+    if (!has_basic_auth_) {
+        response = cpr::Get(url);
+    } else {
+        cpr::Authentication auth{ basicauth_user_, basicauth_password_ };
+        response = cpr::Get(url, auth);
     }
 
-    http_client client(UTF8ToPlatformString(base_url_), client_config);
-    bool succeed = false;
-    bool error = false;
-    EPGStation::Channels channels;
-
-    pplx::task<void> request_task = pplx::create_task([&]() {
-        return client.request(methods::GET, UTF8ToPlatformString(kEPGStationAPIChannels));
-    }).then([&](const http_response& response) {
-        if (response.status_code() != status_codes::OK) {
-            error = true;
-        }
-        return response.extract_utf8string();
-    }).then([&](const std::string& response_string) {
-        if (error) {
-            Log::ErrorF("%s failed: %s", kEPGStationAPIChannels, response_string.c_str());
-            return;
-        }
-        json j = json::parse(response_string);
-        channels = j.get<EPGStation::Channels>();
-        succeed = true;
-    });
-
-    try {
-        request_task.wait();
-    } catch (const std::exception& ex) {
-        Log::ErrorF(LOG_FILE_FUNCTION_MESSAGE(ex.what()));
+    if (response.error) {
+        Log::ErrorF("curl failed for %s: error_code = %d, msg = %s", kEPGStationAPIChannels, response.error.code, response.error.message.c_str());
+        return std::nullopt;
+    } else if (response.status_code >= 400) {
+        Log::ErrorF("%s error: status_code = %d, body = %s", kEPGStationAPIChannels, response.status_code, response.text.c_str());
+        return std::nullopt;
     }
 
-    return succeed ? std::make_optional(std::move(channels)) : std::nullopt;
+    json j = json::parse(response.text);
+    EPGStation::Channels channels = j.get<EPGStation::Channels>();
+
+    return channels;
 }
 
 std::string EPGStationAPI::GetMpegtsLiveStreamPathQuery(int64_t id, int encode_mode) {
